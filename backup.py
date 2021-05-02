58fe050a019dc95e9ed81d98b7335320997a97cd
Ahmet Cemil Yangin <root@ahmetyangin.com>
2021-05-02 22:02:26 +0300

    Add files via upload

---
+++ backup.py
@@ -0,0 +1,33 @@
+import os
+from datetime import datetime
+from shutil import make_archive
+import time
+
+class backup:
+
+    def __init__(self):
+        self.backupDir = "" # zip file save path
+        self.sourceDir = "" # source files path
+    
+    def backupTime(self):
+        date = datetime.now()
+        if("{}".format(date.hour) == "{}".format(21)):
+            return True
+        else:
+            time.sleep(1800)
+            return False
+
+    def zipFile(self,zipName):
+        os.chdir(self.backupDir)
+        make_archive(zipName,"zip",self.sourceDir)
+    
+    def backupStart(self):
+        while True:
+            if self.backupTime():
+                self.zipFile("hurmacinizBackup")
+                break
+        
+
+if __name__ == "__main__":
+    x = backup()
+    x.backupStart()
